991b73ebd8efd2b44a66726f592689d77caf4726
mukit24 <mukithasanpranto4@gmail.com>
2023-05-15 15:57:41 +0600

    template driven form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,8 +9,25 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
   constructor(private router: Router){}
-  
+  @ViewChild('f') form1: NgForm;
+  username = '';
+  isSubmit = false;
+
   onNavigate(){
     this.router.navigate(['/users'], {queryParams: {have_user: true}});
+  }
+
+  onSubmit(){
+    // console.log(this.form1);
+    this.isSubmit = true;
+    this.username = this.form1.value.name;
+    this.form1.reset();
+  }
+
+  onSuggest(){
+    // setValue for setting whole form value
+    this.form1.form.patchValue({
+      name: 'Mukit'
+    })
   }
 }
